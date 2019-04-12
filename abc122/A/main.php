448623b8c87ba38c76b275baeb01fdb47b2e5007
skxeve <skxeve@gmail.com>
2019-04-12 23:09:06 +0900

    ABC122 A.DoubleHelix

---
+++ abc122/A/main.php
@@ -0,0 +1,15 @@
+<?php
+
+$inputs = [];
+while (($item = fgets(STDIN)) !== false) {
+    $inputs[] = $item;
+}
+
+$map = [
+    'A' => 'T',
+    'T' => 'A',
+    'C' => 'G',
+    'G' => 'C',
+];
+$x = trim(array_shift($inputs));
+echo $map[$x];
