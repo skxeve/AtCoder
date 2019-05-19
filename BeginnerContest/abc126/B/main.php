f48832fad71179408db7667873bd669dd202ff82
skxeve <skxeve@gmail.com>
2019-05-19 23:01:00 +0900

    [WA]ABC126 B.YYMM or MMYY

---
+++ BeginnerContest/abc126/B/main.php
@@ -0,0 +1,39 @@
+#!/usr/bin/env php
+<?php
+const YYMM = 'YYMM';
+const MMYY = 'MMYY';
+const BOTH = 'AMBIGUOUS';
+const INVALID = 'NA';
+function genInputs() {
+    while(($input = rtrim(fgets(STDIN))) !== false) {
+        foreach (explode(' ', $input) as $v) {
+            yield $v;
+        }
+    }
+}
+function getNext($gen) {
+    $value = $gen->current();
+    $gen->next();
+    return $value;
+}
+$gen = genInputs();
+
+$S = (int)getNext($gen);
+
+$a = (int)substr($S, 0, 2);
+$b = (int)substr($S, 2);
+
+function valid_month($n) {
+    return ($n >= 1 && $n <= 12);
+}
+
+if (valid_month($a) && valid_month($b)) {
+    echo BOTH;
+} elseif (valid_month($a)) {
+    echo MMYY;
+} elseif (valid_month($b)) {
+    echo YYMM;
+} else {
+    echo INVALID;
+}
+echo PHP_EOL;
